d109d4034939bd4fee9a0acc54101432a9200873
ananthn24 <ananthn24@vt.edu>
2025-03-22 11:46:37 -0400

    data processing phase 1

---
+++ data_processing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+#for processing and combining data by zip code
+
+#combining the data (just writing)
+#still need to organize ZHVI and add more data
+
+desnity_data = pd.read_csv('Population-Density-Final.csv')
+
+home_value_data = pd.read_csv('ZHVI_home_values.csv')
+
+merged_data = pd.merge(desnity_data, home_value_data, on='Zip', how='inner')
+
+#...
+
+merged_data.to_csv('combined_data.csv', index = False)
