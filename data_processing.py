0a4ace29f7591e83c5bc96646baff4412a583cfc
anarayan05 <namanjames4@gmail.com>
2025-04-01 16:08:46 -0400

    final database generated

--- data_processing.py
+++ data_processing.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 #for processing and combining data by zip code
 
+#TASKS: 
+# Decide and finish other growth rate intervals
+# Write into files and merge into combined data.      
+
 #combining the data (just writing)
-#still need to organize ZHVI and add more data
 
 #possibly handle NaN.
 def calculate_growth_rate(df, start_year, end_year):
@@ -20,21 +23,28 @@ density_data = pd.read_csv('Population-Density-Final.csv') #stored as DF
 
 density_data = density_data[['Zip', 'population', 'density', 'City', 'lat', 'long']]
 
-home_value_data = pd.read_csv('C:/Users/naman/Desktop/ZHVI_home_values.csv')
+home_value_data = pd.read_csv('../Databases for CHP/ZHVI_home_values.csv')
 
 zips = home_value_data.iloc[:, 2]
 
-print(zips)
-
 hv_by_year = home_value_data.iloc[:, 9:]
-print(hv_by_year)
 
+last_10_years = calculate_growth_rate(hv_by_year, hv_by_year.shape[1] - 120, hv_by_year.shape[1] - 1)
+last_5_years = calculate_growth_rate(hv_by_year, hv_by_year.shape[1] - 61, hv_by_year.shape[1] - 1)
 overall_growth_rate = calculate_growth_rate(hv_by_year, 9, hv_by_year.shape[1] - 1)
 
-home_value_data_combined = pd.concat([home_value_data[['Zip', 'Metro']], hv_by_year], axis=1)
+growth_df = pd.DataFrame({
+    'Zip': zips,
+    'Five_Year_Growth': last_5_years,
+    'Ten_Year_Growth': last_10_years,
+    'Overall_Growth': overall_growth_rate
+})
 
-merged_data = pd.merge(density_data, home_value_data, on='Zip', how='inner')
+merged_data = pd.merge(home_value_data[['Zip', 'Metro']], growth_df, on='Zip', how='inner')
 
-#merged_data.to_csv('combined_data.csv', index = False)
+#final db
+merged_data = pd.merge(merged_data, density_data, on='Zip', how='inner').dropna()
 
-#...final merge with growth rates
+print(merged_data.columns)
+
+merged_data.to_parquet('../Databases for CHP/final_database.parquet', index=False)
