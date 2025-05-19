7ae556a4ed332cf698498f395bb9c3b5a5251697
anarayan05 <namanjames4@gmail.com>
2025-05-19 18:53:36 -0400

    Reorganized and created city data objects

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,13 @@
 import pandas as pd
+import duckdb
 
 #for processing and combining data by zip code
 
 #TASKS: 
-# Decide and finish other growth rate intervals
-# Write into files and merge into combined data.      
+#map initial dots on map based on data (areas that have been hot)
 
 #combining the data (just writing)
 
-#possibly handle NaN.
 def calculate_growth_rate(df, start_year, end_year):
     growth_rate = []
     for _, row in df.iterrows():
@@ -19,11 +18,11 @@ def calculate_growth_rate(df, start_year, end_year):
     return growth_rate
 
 
-density_data = pd.read_csv('Population-Density-Final.csv') #stored as DF
+density_data = pd.read_csv('Databases for CHP/Population-Density-Final.csv', dtype = {'Zip': str}) #stored as DF
 
 density_data = density_data[['Zip', 'population', 'density', 'City', 'lat', 'long']]
 
-home_value_data = pd.read_csv('../Databases for CHP/ZHVI_home_values.csv')
+home_value_data = pd.read_csv('Databases for CHP/ZHVI_home_values.csv', dtype = {'Zip': str})
 
 zips = home_value_data.iloc[:, 2]
 
@@ -47,4 +46,8 @@ merged_data = pd.merge(merged_data, density_data, on='Zip', how='inner').dropna(
 
 print(merged_data.columns)
 
-merged_data.to_parquet('../Databases for CHP/final_database.parquet', index=False)
+#leading 0s from zip code removed bruh
+
+#merged_data.to_parquet('Databases for CHP/final_database.parquet', index=False)
+
+#dparquet = pd.read_parquet('Databases for CHP/final_database.parquet')
